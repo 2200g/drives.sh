69750ef597632999bfaf00517b297d9a5622ec2b
aadi <aadi@aadi.net.in>
2023-05-07 09:56:19 +0530

    cleaned it, added a test to check if the drive exists.

--- drives.sh
+++ drives.sh
@@ -1,31 +1,28 @@
 #!/bin/sh
-
 lsblk
 
-echo "choose your drive ex: [ /dev/sda ] without the []s"
-read drive
+read -r -p "choose your drive ex: (/dev/sda) without the ()s" drive
 
 UUID=$(sudo blkid | grep "$drive" | grep -oP '(?<= UUID=").*?(?=")')
 
+if [ $? -eq 0 ]; then
+	echo "the UUID of the chosen drive is: $UUID"
+else
+	echo "the drive you selected does not exist."
+	exit
+fi
 
-echo "the UUID of the chosen drive is: $UUID"
+sudo umount $drive && echo "unmounted the drive (if mounted)."
 
-sudo umount $drive
-echo "unmounted the drive (if mounted)."
+cp /etc/fstab ~/fstab.old && echo "backed up /etc/fstab at ~/fstab.old."
 
-cp /etc/fstab ~/fstab.old
-echo "backed up /etc/fstab at ~/fstab.old."
-
-echo "choose your mount point (provide complete dir) ex: [/dev/storage/] without the []s"
-read mountpoint
+read -r -p "choose your mount point (provide complete dir) ex: (/dev/storage/) without the ()s" mountpoint
 sudo mkdir $mountpoint
 
 sudo sh -c "echo \"#$drive\" >> /etc/fstab"
 sudo sh -c "echo \"UUID=$UUID $mountpoint ext4 defaults,nofail 0 2\" >> /etc/fstab"
 echo "added the drive in fstab."
 
-sudo systemctl daemon-reload
-echo "reloaded."
+sudo systemctl daemon-reload && echo "reloaded."
 
-sudo mount -a
-echo "mounted."
+sudo mount -a && echo "mounted."
